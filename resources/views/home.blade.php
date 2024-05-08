ca397bf8ea76b762e379a5e68bd1b4a5bdddc7ad
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-08 13:12:20 +0200

    [add] Hello world

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css'
+        integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg=='
+        crossorigin='anonymous' />
+    <title>Laravel Primi Passi</title>
+</head>
+
+<body>
+    <div class="container my-5">
+        <h1>Hello World</h1>
+    </div>
+</body>
+
+</html>
