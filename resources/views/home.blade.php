015303940e157d6bf7cd35c053d61e35caf3a9b5
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-08 13:38:44 +0200

    [add] header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css'
-        integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg=='
-        crossorigin='anonymous' />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <title>Laravel Primi Passi</title>
 </head>
 
@@ -15,6 +17,7 @@
     <div class="container my-5">
         <h1>Hello World</h1>
         <h2>Welcome to Laravel, {{ $name }} {{ $surname }}!</h2>
+        <a href="http://127.0.0.1:8000/header#">Click to Header</a>
     </div>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/header', function () {
+
+    $text = 'Ciao';
+
+    return view('header', compact('text'));
+})->name('header');
+
 Route::get('/', function () {
 
     $name = 'Ken';
